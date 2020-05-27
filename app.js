847cca1c50765c63425ae34dec4c9e9c7d652bd0
richard <phanr@lafayette.edu>
2020-05-27 13:55:19 -0500

    Added Chart.js

--- app.js
+++ app.js
@@ -114,7 +114,6 @@ function hotTags(country) {
 				$.each(data.items, function(i, item) {
 					var tags = item.snippet.tags;
 					if(tags !== undefined) {
-						console.log(tags);
 						tags.forEach(function(idx) {
 							if(!tagsMap.has(idx)) {
 								tagsMap.set(idx, 1);
@@ -141,8 +140,45 @@ function hotTags(country) {
 				}
 				console.log(largestV);
 				console.log(largestK);
+				$('main').append(`<canvas id="chart" width="400" height="400"></canvas>`);
+				var ctx = document.getElementById('chart').getContext('2d');
+				var myChart = new Chart(ctx, {
+					type: 'bar',
+					data: {
+						labels: largestK,
+						datasets: [{
+							label: 'Tags',
+							data: largestV,
+							backgroundColor: [
+				                'rgba(255, 99, 132, 0.2)',
+				                'rgba(54, 162, 235, 0.2)',
+				                'rgba(255, 206, 86, 0.2)',
+				                'rgba(75, 192, 192, 0.2)',
+				                'rgba(153, 102, 255, 0.2)',
+				                'rgba(255, 159, 64, 0.2)'
+				            ],
+				            borderColor: [
+				                'rgba(255, 99, 132, 1)',
+				                'rgba(54, 162, 235, 1)',
+				                'rgba(255, 206, 86, 1)',
+				                'rgba(75, 192, 192, 1)',
+				                'rgba(153, 102, 255, 1)',
+				                'rgba(255, 159, 64, 1)'
+				            ],
+				            borderWidth: 1
+						}]
+					},
+					options: {
+						scales: {
+							yAxes: [{
+								ticks: {
+									beginAtZero: true
+								}
+							}]
+						}
+					}
+				});
 			});
-			console.log(tagsMap);
 		}
 	});
 }
