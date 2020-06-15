59518ab76ad56fe8345ca79921646fcc52705f5c
richard <phanr@lafayette.edu>
2020-06-15 14:52:40 -0500

    Fix

--- app.js
+++ app.js
@@ -131,6 +131,17 @@ function hotTags(country) {
 						  z-index: 1;
 						}
 
+						.tooltip .tooltiptext::after {
+							content: "";
+							position: absolute;
+							top: 100%;
+							left 50%;
+							margin-left: -5px;
+							border-width: 5px;
+							border-style: solid;
+							border-color: black transparent transparent transparent;
+						}
+
 						.tooltip:hover .tooltiptext {
 						  visibility: visible;
 						}
